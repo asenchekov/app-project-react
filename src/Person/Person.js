785066201368a97c83c0489e4771c5f7aae0fb24
Asen Chekov <asen.chekov@gmail.com>
2019-03-06 18:40:12 +0000

    using styles

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './Person.css';
 
 const person = ( props ) => {
@@ -6,7 +7,7 @@ const person = ( props ) => {
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
+            <input type="text" value={props.name} onChange={props.changed}/>
         </div>
     )
 };
